8784ca4fd5e6b801a2d9cdb6d49741e3e5df13b8
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2020-12-19 19:02:54 +0100

    Add files via upload

---
+++ Tema 1/Ejercicio 1/ejercicio1.java
@@ -0,0 +1,9 @@
+public class ejercicio1 {
+  public static void main (String []args) {
+
+    // Este es el programa más fácil en Java
+    // holaMundo en Java
+    System.out.println ("Hola");
+    System.out.println ("Soy Rafael Aguilar Munoz");
+  }
+}
