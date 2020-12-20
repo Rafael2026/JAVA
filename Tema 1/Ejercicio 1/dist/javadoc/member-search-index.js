381a3321b31b8f1d48500e0ed20a8525faca4037
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2020-12-20 14:23:54 +0100

    Add files via upload

---
+++ Tema 1/Ejercicio 1/dist/javadoc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"ejercicio1","l":"ejercicio1()","url":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"ejercicio1","l":"main(String[])","url":"main(java.lang.String[])"}]
---
+++ Tema 1/Ejercicio 1/src/ejercicio1.java
@@ -0,0 +1,10 @@
+public class ejercicio1 {
+  public static void main (String []args) {
+    
+    // Este es el programa más sencillo en Java
+    // Me llamo holaMundo
+    // Hecho en Java
+    System.out.println ("Hola Mundo");
+    System.out.println ("Soy Rafael Aguilar Munoz");
+  }
+}
