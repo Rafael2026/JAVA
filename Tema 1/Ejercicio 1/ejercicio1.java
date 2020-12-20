5c24ce3fe8bf79092b3fdfc1af6d2cb850c9c57e
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2020-12-20 14:20:14 +0100

    Delete ejercicio1.java

--- Tema 1/Ejercicio 1/ejercicio1.java
+++
@@ -1,9 +0,0 @@
-public class ejercicio1 {
-  public static void main (String []args) {
-
-    // Este es el programa más fácil en Java
-    // holaMundo en Java
-    System.out.println ("Hola");
-    System.out.println ("Soy Rafael Aguilar Munoz");
-  }
-}
