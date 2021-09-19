c73a7d489d773b11dd9fe3052edbe6ee225761ea
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2021-09-19 18:01:27 +0200

    Update ejercicio6.java

--- Tema 1/Ejercicio 6/ejercicio6.java
+++ Tema 1/Ejercicio 6/ejercicio6.java
@@ -2,7 +2,6 @@ public class ejercicio6 {
   public static void main (String[] args ) {
 
     // Pirámide hecha en Java
-
     System.out.println ("    " + "*" + "    ");
     System.out.println ("   " + "***" + "   ");
     System.out.println ("  " + "*****" + "  ");
