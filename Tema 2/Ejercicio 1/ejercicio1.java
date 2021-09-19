8ad89666b563c9d754f94d9f0ecbad859253e088
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2021-09-19 18:14:45 +0200

    Add files via upload

---
+++ Tema 2/Ejercicio 1/ejercicio1.java
@@ -0,0 +1,19 @@
+public class ejercicio1 {
+  public static void main (String []args) {
+
+    int x = 144;
+    int y = 999;
+
+    System.out.println ("suma: " + (x + y));
+    System.out.println ();
+
+    System.out.println ("resta: " + (x - y));
+    System.out.println ();
+
+    System.out.println ("multiplicacion: " + (x * y));
+    System.out.println ();
+
+    System.out.println ("division: " + (x / y));
+    System.out.println ();
+  }
+}
