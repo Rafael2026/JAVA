5b7df3cf256b3667b0481c97a21f9ebba348c70f
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2021-09-19 18:22:50 +0200

    Add files via upload

--- Tema 2/Ejercicio 4/ejercicio4.java
+++ Tema 2/Ejercicio 4/ejercicio4.java
@@ -1,16 +1,10 @@
 public class ejercicio4 {
   public static void main (String []args ){
-  
-    double cantidadEnEuros;
 
-    System.out.print("Introducir una cantidad en euros: ");
-    cantidadEnEuros = Double.parseDouble (System.console().readLine());
-  
-    double total;
-    total = (cantidadEnEuros * 166.386);
-    
-    System.out.println ();
-    System.out.println ("Euros introducidos: " + cantidadEnEuros);
-    System.out.println ("Pesetas: " + total);
+    double cantidadEnEuros = 50;
+    double total = cantidadEnEuros * 166.386;
+
+    System.out.print ("Euros introducidos: " + cantidadEnEuros + "\n");
+    System.out.print ("Pesetas: " + total);
   }
 }
