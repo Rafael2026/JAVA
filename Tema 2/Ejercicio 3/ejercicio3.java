bc69cd8381ef2de2acce5d83417ce3229d2190c1
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2021-09-19 18:20:26 +0200

    Add files via upload

--- Tema 2/Ejercicio 3/ejercicio3.java
+++ Tema 2/Ejercicio 3/ejercicio3.java
@@ -1,18 +1,12 @@
 public class ejercicio3 {
   public static void main (String []args) {
-    
-    String nombreApellidos = "Rafael Aguilar Munoz";
-    String direccion = "C/Ceres numero 8, Campanillas";
-    long numeroTelefono = 609213851;
-    
-    System.out.println ("Hecho por " + nombreApellidos);
-    System.out.println ();
-    
-    System.out.println ("Vivo en " + direccion);
-    System.out.println ();
-    
-    System.out.println ("Mi numero de telefono es " + numeroTelefono);
-    System.out.println ();
-    
+
+    String nombre = "Rafael";
+    String direccion = "C/Ceres Nº 8, Campanillas";
+    long telefono = 609213851;
+
+    System.out.print ("Hecho por " + nombre + "\n");
+    System.out.print ("Vivo en " + direccion + "\n");
+    System.out.print ("Mi numero de telefono es " + telefono);
   }
 }
