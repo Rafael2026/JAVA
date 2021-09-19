0fae2bca0143408888587e82939cd39a556ccacb
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2021-09-19 18:01:44 +0200

    Update ejercicio7.java

--- Tema 1/Ejercicio 7/ejercicio7.java
+++ Tema 1/Ejercicio 7/ejercicio7.java
@@ -2,7 +2,6 @@ public class ejercicio7 {
   public static void main (String[] args ) {
 
     // Pirámide hueca hecha en Java
-
     System.out.println ("    " + "*" + "    ");
     System.out.println ("   " + "*" + " " + "*" + "   ");
     System.out.println ("  " + "*" + "   " + "*" + "  ");
