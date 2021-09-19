68b1b70bad963ca80f42521d7dfc3f6c9dca7ecb
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2021-09-19 18:07:33 +0200

    Add files via upload

--- Tema 1/Ejercicio 9/ejercicio9.java
+++ Tema 1/Ejercicio 9/ejercicio9.java
@@ -1,40 +1,40 @@
 public class ejercicio9 {
-	public static void main(String []args) {
+  public static void main(String []args) {
 
-		int x = 0;
-		int y = 0;
-		int base;
-		int altura;
+    int x = 0;
+    int y = 0;
+    int base;
+    int altura;
 
-		// Anchura de la bandera
-		System.out.print ("Introducir base: ");
-		base = Integer.parseInt (System.console().readLine());
+    // Anchura de la bandera
+    System.out.print ("Introducir base: ");
+    base = Integer.parseInt (System.console().readLine());
 
-		System.out.println ();
+    System.out.println ();
 
-		// Altura de la bandera
-		System.out.print ("Introducir altura: ");
-		altura = Integer.parseInt (System.console().readLine());
+    // Altura de la bandera
+    System.out.print ("Introducir altura: ");
+    altura = Integer.parseInt (System.console().readLine());
 
-		System.out.println ();
+    System.out.println ();
 
-		// Pintar la bandera
-		do {
+    // Pintar la bandera
+    do {
 
       System.out.print ("|");
 
-			for (x = 0; x < base; x++) {
+      for (x = 0; x < base; x++) {
 
         System.out.print ("*");
 
-				if (x == base - 1) {
+        if (x == base - 1) {
           System.out.print ("|");
-					System.out.println ();
-				}
-			}
+          System.out.println ();
+        }
+      }
 
       y++;
 
-		} while (y < altura);
-	}
+    } while (y < altura);
+  }
 }
