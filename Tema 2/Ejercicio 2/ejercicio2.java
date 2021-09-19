c0e635d2a4401262fd97775a9d70782b8e32730d
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2021-09-19 18:17:49 +0200

    Add files via upload

--- Tema 2/Ejercicio 2/ejercicio2.java
+++ Tema 2/Ejercicio 2/ejercicio2.java
@@ -1,5 +1,6 @@
 public class ejercicio2 {
   public static void main (String[] args) {
-    System.out.print ("Hecho por " + "Rafael Aguilar Munoz");
+    String nombre = "Rafael";
+    System.out.print ("Hecho por " + nombre);
   }
 }
