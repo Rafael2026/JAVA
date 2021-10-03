bf4e42b3c96018d586c29dd4c1a2996119c6835d
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2021-10-03 17:34:33 +0200

    Update ejercicio5.java

--- Tema 2/Ejercicio 5/ejercicio5.java
+++ Tema 2/Ejercicio 5/ejercicio5.java
@@ -2,9 +2,9 @@ public class ejercicio5 {
   public static void main (String[] args ){
 
     double cantidadEnPesetas = 1000000;
-    double total = cantidadEnPesetas / 166.386;
+    double total = (cantidadEnPesetas / 166.386);
 
-    System.out.print ("Pesetas: " + cantidadEnPesetas + "\n");
-    System.out.print ("Euros: " + total);
+    System.out.println ("Pesetas: " + cantidadEnPesetas);
+    System.out.println ("Euros: " + total);
   }
 }
