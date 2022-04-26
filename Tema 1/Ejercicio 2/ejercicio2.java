0555b749d3a6439be2ebe69f3afd2f9deef27729
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2022-04-26 14:54:34 +0200

    Add files via upload

--- Tema 1/Ejercicio 2/ejercicio2.java
+++ Tema 1/Ejercicio 2/ejercicio2.java
@@ -1,16 +1,7 @@
 public class ejercicio2 {
   public static void main (String[] args) {
-
-    System.out.println ("Hola"); // Programa más sencillo hecho en Java
-    System.out.println ();
-
-    System.out.println ("Soy Rafael Aguilar Munoz"); // Mi nombre y mis apellidos
-    System.out.println ();
-
-    System.out.println ("Direccion: Campanillas, C/Ceres, Numero 8"); // Vivienda
-    System.out.println ();
-
-    System.out.println ("Numero de telefono: 609 21 38 51"); // Mi número de teléfono
-    System.out.println ();
+    System.out.println("Soy Rafael Aguilar Munoz"); // Nombre y apellidos
+    System.out.println("Direccion: Campanillas, C/Ceres, Numero 8"); // Vivienda
+    System.out.println("Numero de telefono: 609 21 38 51"); // Número de teléfono
   }
 }
