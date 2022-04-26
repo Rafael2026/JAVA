5fecf3613705f79fa36f63b06ad752f1b912449f
Rafael Aguilar Muñoz <55810413+Rafael2026@users.noreply.github.com>
2022-04-26 14:43:49 +0200

    Update ejercicio10.java

--- Tema 1/Ejercicio 10/ejercicio10.java
+++ Tema 1/Ejercicio 10/ejercicio10.java
@@ -1,30 +1,23 @@
 public class ejercicio10 {
-  public static void main(String []args) {
+  public static void main(String[] args) {
 
-    int x = 0;
     int y = 0;
-    int base;
-    int altura;
+    String azul = "\33[34m"; // Color de la bandera
 
-    // Color de la bandera
-    String azul = "\33[34m";
+    System.out.print("Introducir base: ");
+    int base = Integer.parseInt (System.console().readLine()); // Anchura de la bandera
 
-    // Anchura de la bandera
-    System.out.print ("Introducir base: ");
-    base = Integer.parseInt (System.console().readLine());
+    System.out.print("Introducir altura: ");
+    int altura = Integer.parseInt (System.console().readLine()); // Altura de la bandera
 
-    // Altura de la bandera
-    System.out.print ("Introducir altura: ");
-    altura = Integer.parseInt (System.console().readLine());
-
-    System.out.println ();
+    System.out.println();
 
     // Pintar la bandera
     do {
 
       System.out.print (azul + "|");
 
-      for (x = 0; x < base; x++) {
+      for (int x = 0; x < base; x++) {
 
         System.out.print ("*");
 
